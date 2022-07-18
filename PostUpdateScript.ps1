de14e3f18bcc5398da79f93408e5dca43704d9e0
wikiped <wikiped@ya.ru>
2022-07-18 18:39:48 +0300

    Remove redundant Split-Path in PostUpdateScript.ps1

--- PostUpdateScript.ps1
+++ PostUpdateScript.ps1
@@ -8,7 +8,7 @@ $n = Split-Path $PSScriptRoot -Leaf
 [version]$VersionToMigrateConfig = '0.20'
 
 if ($VersionBeforeUpdate -lt $VersionToMigrateConfig -and $VersionAfterUpdate -ge $VersionToMigrateConfig) {
-    $script = Join-Path (Split-path $PSScriptRoot) '.\Scripts\Powershell\MigrateConfigFiles.ps1' -Resolve
+    $script = Join-Path $PSScriptRoot '.\Scripts\Powershell\MigrateConfigFiles.ps1' -Resolve
     Write-Debug "${n}: Invoking '$script'..."
     & $script @PSBoundParameters
 }
